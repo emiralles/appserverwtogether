90d2cb10993786b18c9d0cb8d571b189879784fd
emiralles <emiralles_yataco@hotmail.com>
2017-05-31 00:34:40 +0200

    t.. commit

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
-var socket = require('socket.io'),
-    http = require('http'),
-    server = http.createServer(),
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var port = process.env.PORT || 3000;
 
-socket = socket.listen(server);
-
-socket.on('connection',function(connection){
-    console.log('User Connected');
-    connection.on('message',function(msg){
-        socket.emit('message',msg );
-    });
+server.listen(port, function () {
+  console.log('Server listening at port %d', port);
 });
+// Routing
+app.use(express.static(__dirname + '/public'));
 
-server.listen(3000, ()=>{
-    console.log('Server started');
-});
+// var socket = require('socket.io'),
+//     http = require('http'),
+//     server = http.createServer(),
+//
+// socket = socket.listen(server);
+//
+// socket.on('connection',function(connection){
+//     console.log('User Connected');
+//     connection.on('message',function(msg){
+//         socket.emit('message',msg );
+//     });
+// });
+//
+// server.listen(3000, ()=>{
+//     console.log('Server started');
+// });
