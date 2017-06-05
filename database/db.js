9b79429adbca0bd33123f00f15de292c45c95f7d
emiralles <emiralles_yataco@hotmail.com>
2017-06-05 23:59:22 +0200

    another by commit

---
+++ database/db.js
@@ -0,0 +1,60 @@
+'use strict';
+
+var firebase = require("firebase-admin");
+var serviceAccount = require("wtogether-4855e-firebase-adminsdk-44scg-23a03dea32.json");
+
+var structUser = {
+    userId: '',
+    username: '',
+    email: '',
+    room: ''
+};
+
+var structMessage = {
+  userIdSend:'',
+  userIdTo:'',
+  room:'',
+  message: ''
+};
+
+var structRoom = {
+  userIdPropietario:'',
+  userIdInvitado:'',
+  room:''
+};
+
+firebase.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://wtogether-4855e.firebaseio.com"
+});
+
+var db = firebase.database();
+
+
+function writeUser(userId, name, email, room) {
+  db.ref('users/' + userId).set({
+    userId: userId,
+    username: name,
+    email: email,
+    room: room
+  });
+};
+
+
+function readUser(userId){
+  var users = db.ref('users/');
+  
+  users.orderByChild('userId').equalTo(userId).on('value',function(snapshot){
+    structUser.userId = snapshot.userId,
+    structUser.username = snapshot.username,
+    structUser.email = snapshot.email,
+    structUser.room = snapshot.room
+
+  });
+
+};
+
+
+
+module.exports="dbs";
+
--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+'use strict';
+
+var dbs = require("/home/emir/nodeJS/chatserver/database/db.js");
+
 var socket = require('socket.io'),
     http = require('http'),
     server = http.createServer(),
@@ -9,8 +13,24 @@ socket.on('connection',function(connection){
     connection.on('message',function(msg){
         socket.emit('message',msg );
     });
+
+    socket.on('connection',function(socket){
+        var room = dbs.structRoom;
+        room.userIdPropiertario = userIDP;
+        room.userIdInvitado = userIdI;
+        room.room = 'room_'+ userIdI + "_" + "userIdP";
+        socket.join(room.room);
+    });
+
+    
+});
+
+io.on('connection', function(socket){
+  socket.join('some room');
 });
 
 server.listen(3000, ()=>{
     console.log('Server started');
 });
+
+
