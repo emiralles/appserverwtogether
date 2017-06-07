703e2b0459e5f34469673e492c9078a143bea17d
emiralles <emiralles_yataco@hotmail.com>
2017-06-07 02:39:41 +0200

    ait ..

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ var server = require('http').Server(app);
 var socket = require('socket.io')(server);
 var bodyparser = require('body-parser');
 var dbs = require('./database/db.js');
-
+var room = dbs.structRoom;
+var roomPropietary = '';
     //http = require('http'),
     //server = http.createServer(),
 app.use(bodyparser.json());
@@ -16,22 +17,33 @@ app.get('/',function(req,res){
 //socket = socket.listen(server);
 
 socket.on('connection',function(connection){
+    
     console.log('User Connected');
+    
+    connection.on('myroom',function(myroom,username){
+        roomPropietary = username;
+        socket.join(roomPropietary);
+    });
+
     connection.on('message',function(msg){
         socket.emit('message',msg );
     });
 
-    socket.on('connection',function(socket){
-        var room = dbs.structRoom;
+    connection.on('openroom',function(oproom){
+        
         room.userIdPropiertario = userIDP;
         room.userIdInvitado = userIdI;
         room.room = 'room_'+ userIdI + "_" + "userIdP";
         socket.join(room.room);
+        
+    });
+
+    connection.on('addUser',function(userId,username,email){
+        dbs.writeUser(userId,username,email,roomPropietary);
     });
 
     
 });
-
 
 server.listen(3000, function(){
     console.log('Server started');
